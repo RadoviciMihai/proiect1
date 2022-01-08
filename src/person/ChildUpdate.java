aac2574be371c8ff8f9fa8b58a94865d5ceb6c04
Mihai <mihai.radovici@stud.acs.upb.ro>
2022-01-08 14:16:05 +0200

    obiectul ChildUpdate

---
+++ src/person/ChildUpdate.java
@@ -0,0 +1,43 @@
+package person;
+
+import enums.Category;
+
+import java.util.List;
+
+class ChildUpdate {
+    private int id;
+    private double niceScore;
+    private List<Category> giftsPreferences;
+
+    ChildUpdate(final int id,
+                final double niceScore,
+                final List<Category> giftsPreferences) {
+        this.id = id;
+        this.niceScore = niceScore;
+        this.giftsPreferences = giftsPreferences;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    public double getNiceScore() {
+        return niceScore;
+    }
+
+    public void setNiceScore(final double niceScore) {
+        this.niceScore = niceScore;
+    }
+
+    public List<Category> getGiftsPreferences() {
+        return giftsPreferences;
+    }
+
+    public void setGiftsPreferences(final List<Category> giftsPreferences) {
+        this.giftsPreferences = giftsPreferences;
+    }
+}

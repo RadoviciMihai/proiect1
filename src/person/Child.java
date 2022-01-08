1c63dcd15b04d62346d9e9734f773b813926e34e
Mihai <mihai.radovici@stud.acs.upb.ro>
2022-01-08 13:54:48 +0200

    obiectul child

---
+++ src/person/Child.java
@@ -0,0 +1,76 @@
+package person;
+
+import enums.Category;
+
+import java.util.List;
+
+final class Child {
+    private int id;
+    private String lastName;
+    private int age;
+    private String city;
+    private double niceScore;
+    private List<Category> giftsPreferences;
+
+    Child(final int id,
+                 final String lastName,
+                 final int age,
+                 final String city,
+                 final double niceScore,
+                 final List<Category> giftsPreferences) {
+        this.id = id;
+        this.lastName = lastName;
+        this.age = age;
+        this.city = city;
+        this.niceScore = niceScore;
+        this.giftsPreferences = giftsPreferences;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(final int age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(final String city) {
+        this.city = city;
+    }
+
+    public double getNiceScore() {
+        return niceScore;
+    }
+
+    public void setNiceScore(final double niceScore) {
+        this.niceScore = niceScore;
+    }
+
+    public List<Category> getGiftsPreferences() {
+        return giftsPreferences;
+    }
+
+    public void setGiftsPreferences(final List<Category> giftsPreferences) {
+        this.giftsPreferences = giftsPreferences;
+    }
+}
